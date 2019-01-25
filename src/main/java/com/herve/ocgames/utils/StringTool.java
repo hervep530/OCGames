7b97ae5984341295ef8fc919cac5a3b548932f89
hervep530 <hervep6963@gmail.com>
2019-01-25 08:31:12 +0100

    PropertyHelperTest need StringTool so implements StringToolTest and add methods without code in StringTool

--- src/main/java/com/herve/ocgames/utils/StringTool.java
+++ src/main/java/com/herve/ocgames/utils/StringTool.java
@@ -1,4 +1,12 @@
 package com.herve.ocgames.utils;
 
 public class StringTool {
+    public static String arrayReplace(String test, String[][] substitutionTable) {
+    }
+
+    public static boolean match(String s, String s1) {
+    }
+
+    public static boolean matchSpecificRule(String currentString, String minMaxOfDigit, int i, int i1, int i2) {
+    }
 }
--- src/test/java/com/herve/ocgames/utils/StringToolTest.java
+++ src/test/java/com/herve/ocgames/utils/StringToolTest.java
@@ -2,16 +2,91 @@ package com.herve.ocgames.utils;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringToolTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
     @BeforeEach
-    void setUp() {
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
     }
 
     @AfterEach
-    void tearDown() {
+    public void restoreStreams() {
+        System.setOut(System.out);
     }
+
+    @Test
+    public void Given_StringContainsStringToReplace_When_StringReplaceAll_Then_ModifyString(){
+        String test = "je dors dans un lit en paille";
+        test = test.replaceAll("paille", "pierre");
+        assertEquals("je dors dans un lit en pierre", test);
+    }
+
+    @Test
+    public void Given_StringDoesntContainsStringToReplace_When_StringReplace_Then_DoNotModifyString(){
+        String test = "je dors dans un lit en paille";
+        test.replace("mousse", "pierre");
+        assertEquals("je dors dans un lit en paille", test);
+    }
+
+    @Test
+    public void Given_StringContainsStringToReplace_When_StringOperationsArrayReplace1Array_Then_ModifyString(){
+        String correct = "je dors dans un lit en bois brun fabriqué par l'ébeniste";
+        String test = "je mange dans un panier en pierre bleue peinte par le forgeron";
+        String[][] substitutionTable = {{"mange", "dors"}, {"panier", "lit"}, {"pierre", "bois"}, {"bleue", "brun"},
+                {"peinte","fabriqué"}, {"le forgeron","l'ébeniste"}};
+        assertEquals(correct, StringTool.arrayReplace(test, substitutionTable));
+    }
+
+    @Test
+    public void Given_StringContainsMultipleEqualAndTheMatchingPattern_When_StringOperationsMatch_Then_ReturnTrue(){
+        boolean isMatch = StringTool.match("====", "^={4,}$");
+        assertEquals(true, isMatch);
+    }
+
+    @Test
+    public void Given_StringWithSpecialCharsAndTheMatchingPattern_When_StringOperationsMatch_Then_ReturnTrue(){
+        String currentString = "{\"operation\";(98+2)*45 -> 4500}";
+        String pattern = "^\\{\\\"operation\\\";\\([0-9]{1,3}\\+[0-9]{1,2}\\)\\*[0-9]{1,4} -> [0-9]{1,}\\}$";
+        boolean isMatch = StringTool.match(currentString, pattern);
+        assertEquals(true, isMatch);
+    }
+
+    @Test
+    public void Given_StringWithSpecialCharsAndNotMatchingPattern_When_StringOperationsMatch_Then_ReturnFalse(){
+        String currentString = "(98+2)*45 -> 4500";
+        String pattern = "([0-9]{1,3}+[0-9]{1,3})*100 -> [0-9]{1,9}";
+        boolean isMatch = StringTool.match(currentString, pattern);
+        assertEquals(false, isMatch);
+    }
+
+    @Test
+    public void Given_StringWithGoodNumberOccurences_When_MatchSpecificRuleWithMinMax_Then_ReturnTrue(){
+        String currentString = "04351";
+        boolean isMatch = StringTool.matchSpecificRule(currentString,"minMaxOfDigit",6,0,2);
+        assertEquals(true, isMatch);
+    }
+
+    @Test
+    public void Given_StringWithMoreThanMaxNumberOccurence_When_MatchSpecificRuleWithMinMax_Then_ReturnFalse(){
+        String currentString = "0434421";
+        boolean isMatch = StringTool.matchSpecificRule(currentString,"minMaxOfDigit",8,0,2);
+        assertEquals(false, isMatch);
+    }
+
+    @Test
+    public void Given_StringWithLessThanMinNumberOccurences_When_MatchSpecificRuleWithMinMax_Then_ReturnFalse(){
+        String currentString = "04351430297";
+        boolean isMatch = StringTool.matchSpecificRule(currentString,"minMaxOfDigit",10,1,2);
+        assertEquals(false, isMatch);
+    }
+
 }
