fe4461fb2c8a702a6b324ed6d47475705a24eccf
hervep530 <hervep6963@gmail.com>
2019-02-01 16:54:26 +0100

    Implement few tests in UserInteractionTest Implement UserInteraction

--- src/main/java/com/herve/ocgames/utils/UserInteraction.java
+++ src/main/java/com/herve/ocgames/utils/UserInteraction.java
@@ -1,8 +1,12 @@
 package com.herve.ocgames.utils;
 
+import com.herve.ocgames.core.PropertyHelper;
 import org.apache.log4j.Logger;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class UserInteraction {
 
@@ -20,7 +24,41 @@ public class UserInteraction {
      * @return
      */
     public static int promptSelect(String category, String[] responses, boolean showResponse) {
-        return 0;
+        String[][] langSubstitutes = new String[][] {{"VAR_CATEGORY", category}};
+        System.out.print(String.format(lang("select.title", langSubstitutes)));
+        for (int i = 1; i <= responses.length; i++)
+            System.out.println(i + " - " + responses[i - 1]);
+        System.out.print(String.format(lang("select.question", langSubstitutes)));
+        int nbResponse;
+        boolean responseIsGood;
+        do {
+            try {
+                nbResponse = sc.nextInt();
+            } catch (InputMismatchException e) {
+                nbResponse = -1;
+                sc.next();
+            } catch (NoSuchElementException e) {
+                nbResponse = -1;
+                sc.next();
+            }
+            responseIsGood = (nbResponse >= 1 && nbResponse <= responses.length);
+            if (responseIsGood) {
+                if (showResponse) {
+                    String[][] answerSubstitutes = new String[][]{
+                            {"VAR_CATEGORY", category},
+                            {"VAR_ANSWER", responses[nbResponse - 1]}};
+                    System.out.print(String.format(lang("select.rightAnswer", langSubstitutes)));
+                }
+            } else {
+                boolean isVowel = "aeiouy".contains(Character.toString(category.charAt(0)));
+                if (isVowel)
+                    System.out.print(String.format(lang("select.invalidCategoryVowel", langSubstitutes)));
+                else
+                    System.out.print(String.format(lang("select.invalidCategoryConsonant", langSubstitutes)));
+            }
+        } while (!responseIsGood);
+        // On ne retourne par nbResponse - 1 . On ne retourne donc jamais la valeur 0
+        return nbResponse;
     }
 
 
@@ -31,29 +69,102 @@ public class UserInteraction {
      * @param showResponse  Affichage de la confirmation de la réponse
      * @return
      */
-    public static String promptInput(String question, String patternInput, boolean showResponse) {
-        return "";
+    public static String promptInput(String question, String patternInput, String color, boolean showResponse) {
+        //System.out.print(String.format(question));
+        UserInteraction.displayMessage(question, color);
+        String strResponse;
+        Pattern pattern = Pattern.compile(patternInput);
+
+        boolean responseIsGood = false;
+        do {
+            strResponse ="";
+            try {
+                strResponse = sc.next();
+            } catch (NoSuchElementException e) {
+                strResponse = "";
+                //sc.nextLine();
+            }
+            if (StringTool.match(strResponse, patternInput)) {
+                String[][] langSubstitutes = new String[][]{{"VAR_RESPONSE",strResponse}};
+                if (showResponse) System.out.print(String.format(lang("input.validAnswer", langSubstitutes)));
+                responseIsGood = true;
+            } else {
+                System.out.print(String.format(lang("input.invalidAnswer")));
+            }
+        } while (!responseIsGood);
+        return strResponse;
     }
 
-    public static String promptInput(String question, String patternInput, String specificRuleName, Integer[] ruleParameters, boolean showResponse) {
-        return "";
+
+    public static String promptInput(String question, String patternInput, String color, String specificRuleName, Integer[] ruleParameters, boolean showResponse) {
+        //System.out.print(String.format(question));
+        UserInteraction.displayMessage(question, color);
+        String strResponse;
+        Pattern pattern = Pattern.compile(patternInput);
+        boolean responseIsGood;
+        do {
+            strResponse ="";
+            responseIsGood = true;
+            try {
+                strResponse = sc.next();
+            } catch (NoSuchElementException e) {
+                strResponse = "";
+            }
+            if (! StringTool.match(strResponse, patternInput) ||
+                    ! StringTool.matchSpecificDigitRule(strResponse, specificRuleName, ruleParameters))
+                responseIsGood = false;
+            if (responseIsGood) {
+                String[][] langSubstitutes = new String[][]{{"VAR_RESPONSE",strResponse}};
+                if (showResponse) System.out.print(String.format(lang("input.validAnswer", langSubstitutes)));
+            } else {
+                System.out.print(String.format(lang("input.invalidAnswer")));
+            }
+        } while (!responseIsGood);
+        return strResponse;
+    }
+
+    public static void initializeScanner(){
+        sc = new Scanner(System.in);
     }
 
     public static void displayMessage(Response response) {
-        return;
+        String message = "";
+        // On prend en priorité le message d'erreur, puis le message applicatif
+        message = response.getErrMessage();
+        if (message.contentEquals("")) message = response.getMessage();
+        // S'il y a un message on l'affiche
+        if ( ! message.contentEquals("")) System.out.print(String.format(message));
     }
 
     public static void displayMessage(Response response, String textColor) {
-        return;
+        String message = "";
+        // On prend en priorité le message d'erreur, puis le message applicatif
+        message = response.getErrMessage();
+        if (message.contentEquals("")) message = Text.effect(response.getMessage(),textColor);
+        // S'il y a un message on l'affiche
+        if ( ! message.contentEquals("")) System.out.print(String.format(message));
+    }
+
+    public static void clearConsole(){
+        System.out.print(String.format(Text.CLEAR_SCREEN));
     }
 
     public static void displayMessage(String message) {
-        System.out.println(String.format(message));
+        System.out.print(String.format(message));
     }
 
     public static void displayMessage(String message, String textColor) {
-        System.out.println(String.format(textColor + message + Text.RESET));
+        System.out.print(String.format(Text.effect(message, textColor)));
     }
+
+    private static String lang(String key){
+        return PropertyHelper.language(key);
+    }
+
+    private static String lang(String key, String[][] arraySubstitutions){
+        return PropertyHelper.language(key, arraySubstitutions);
+    }
+
 
 
 }
--- src/test/java/com/herve/ocgames/utils/UserInteractionTest.java
+++ src/test/java/com/herve/ocgames/utils/UserInteractionTest.java
@@ -1,5 +1,6 @@
 package com.herve.ocgames.utils;
 
+import com.herve.ocgames.core.PropertyHelper;
 import com.herve.ocgames.utils.UserInteraction;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserInteractionTest {
 
+
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @BeforeEach
@@ -24,6 +26,93 @@ class UserInteractionTest {
     void restoreStreams() {
         System.setOut(System.out);
     }
+
+
+    protected String lang(String key, String[][] arraySubstitutions){
+        return PropertyHelper.language(key, arraySubstitutions);
+    }
+
+    private String validAnswer(){
+        return "";
+    }
+
+    private String invalidAnswer(){
+        return "";
+    }
+
+    @Test
+    void Given_ResponseNotMatchingAndResponseMatching_When_AskCodeWithPromptInput_Then_DisplayErrorAndGoodResponse (){
+        System.setIn(new ByteArrayInputStream("123soleil\n135malin\n".getBytes()));
+        String strResponse = UserInteraction.promptInput("Quel est le code secret?",
+                ".*3[0-9]\\w*li.*", "reset", true);
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals(true, output[0].contains("code secret"));
+        assertEquals("Cette tentative n'est pas valide. Veuillez effectuez une nouvelle saisie.", output[1]);
+        assertEquals("Votre nouvelle tentative est 135malin", output[2]);
+    }
+
+    @Test
+    void Given_BadResponseAndResponse4_When_AskAboutColorsWithFiveResponses_Then_DisplayErrorAndGoodResponse() {
+        System.setIn(new ByteArrayInputStream("12\n4\n".getBytes()));
+        String[] responses = {"Rouge", "Bleu", "Jaune", "Vert", "Gris"};
+        int intResponse = UserInteraction.promptSelect("couleur", responses, true);
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals(true, output[0].contains("couleur"));
+        assertEquals("Vous n'avez pas choisi de couleur parmi les choix proposés", output[7]);
+        assertEquals("Vous avez choisi comme couleur : Vert", output[8]);
+    }
+
+    @Test
+    void Given_3NotMatchingInputAndOneGood_When_PromptInput5ArithmeticSymbols_Then_Display3ErrorAndGoodResponse() {
+        System.setIn(new ByteArrayInputStream("44444\n++-=\naeixoee\n-==++\n".getBytes()));
+        String strResponse = UserInteraction.promptInput("Evaluez la tentative avec + = et - ?",
+                "^[-\\+=]{5}$", "reset", true);
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertTrue(output[0].contains("Evaluez la tentative"));
+        assertTrue(output[1].contains("pas valide."));
+        assertTrue(output[2].contains("pas valide."));
+        assertTrue(output[3].contains("pas valide."));
+        assertEquals("Votre nouvelle tentative est -==++", output[4]);
+    }
+
+    @Test
+    void Given_InputWithDigitsOccurencesInRange_When_PromptInput_Then_GoodResponse (){
+        System.setIn(new ByteArrayInputStream("35723\n".getBytes()));
+        String question = "Quel est le code secret?";
+        String matchingPattern = "[0-7]{5}";
+        String strResponse = UserInteraction.promptInput(question, matchingPattern, "reset",
+                "digitMaxRepeat", new Integer[]{8,0,2},true);
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertTrue(output[0].contains("code secret"));
+        assertEquals("Votre nouvelle tentative est 35723", output[1]);
+    }
+
+    @Test
+    void Given_InputWithDigitsOccurencesLessThanMinAndInRange_When_PromptInput_Then_AskRetryAndGoodResponse (){
+        System.setIn(new ByteArrayInputStream("35221003\n01234545\n".getBytes()));
+        String question = "Quel est le code secret?";
+        String matchingPattern = "[0-5]{8}";
+        String strResponse = UserInteraction.promptInput(question, matchingPattern, "reset",
+                "minMaxOfDigit", new Integer[]{6,1,2},true);
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertTrue(output[0].contains("code secret"));
+        assertEquals("Cette tentative n'est pas valide. Veuillez effectuez une nouvelle saisie.", output[1]);
+        assertEquals("Votre nouvelle tentative est 01234545", output[2]);
+    }
+
+    @Test
+    void Given_InputWithDigitsOccurencesMoreThanMaxAndInRange_When_PromptInput_Then_AskRetryAndGoodResponse (){
+        System.setIn(new ByteArrayInputStream("3537823\n354721\n".getBytes()));
+        String question = "Quel est le code secret?";
+        String matchingPattern = "[0-7]{6}";
+        String strResponse = UserInteraction.promptInput(question, matchingPattern, "reset",
+                "minMaxOfDigit", new Integer[]{8,0,2},true);
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertTrue(output[0].contains("code secret"));
+        assertEquals("Cette tentative n'est pas valide. Veuillez effectuez une nouvelle saisie.", output[1]);
+        assertEquals("Votre nouvelle tentative est 354721", output[2]);
+    }
+
 
 
 }
