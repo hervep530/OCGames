4ead093501345d6899d080c9c45ae3fb5fe499f9
hervep530 <hervep6963@gmail.com>
2019-02-02 08:24:07 +0100

    In PlayerPlusMoins : - add specific constructor

--- src/main/java/com/herve/ocgames/plusmoins/PlayerPlusMoins.java
+++ src/main/java/com/herve/ocgames/plusmoins/PlayerPlusMoins.java
@@ -3,4 +3,22 @@ package com.herve.ocgames.plusmoins;
 import com.herve.ocgames.core.Player;
 
 public class PlayerPlusMoins extends Player {
+
+    /*
+     * Constructor
+     */
+
+    public PlayerPlusMoins() {
+        super();
+        this.codeGenerator = new CodeGeneratorPlusMoins();
+        this.codeChecker = new CodeCheckerPlusMoins();
+        //this.extendedBeginMessage = "extendedMessage";
+    }
+
+    public PlayerPlusMoins(String testWithoutRandom) {
+        super();
+        this.codeGenerator = new CodeGeneratorPlusMoins(testWithoutRandom);
+        this.codeChecker = new CodeCheckerPlusMoins();
+    }
+
 }
