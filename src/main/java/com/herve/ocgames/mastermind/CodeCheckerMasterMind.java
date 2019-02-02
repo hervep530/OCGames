ce97a036cd9323f0d4bcd1d9110e356eea44ec23
hervep530 <hervep6963@gmail.com>
2019-02-02 08:01:07 +0100

    In CodeCheckrMasterMind : add methods which do the work (generateEvaluation, askPlayerEvaluation, askRefereeControl)
    Get CodeCheckerMasterMindTest from old version :
    put all in comment for the moment

--- src/main/java/com/herve/ocgames/mastermind/CodeCheckerMasterMind.java
+++ src/main/java/com/herve/ocgames/mastermind/CodeCheckerMasterMind.java
@@ -1,4 +1,159 @@
 package com.herve.ocgames.mastermind;
 
-public class CodeCheckerMasterMind {
+import com.herve.ocgames.core.CodeChecker;
+import com.herve.ocgames.core.GameCache;
+import com.herve.ocgames.core.PropertyHelper;
+import com.herve.ocgames.utils.Response;
+import com.herve.ocgames.utils.StringTool;
+import com.herve.ocgames.utils.UserInteraction;
+
+public class CodeCheckerMasterMind extends CodeChecker {
+
+    /*
+     * Constructor
+     */
+
+    public CodeCheckerMasterMind(){
+        super();
+        this.debugVerbosity = 2 ;
+    }
+
+    /*
+     * Partie Metier
+     */
+
+    @Override
+    public Response generateEvaluation(String attempt, String secret) {
+        if (GameCache.isFailed()) return null;
+        // Initialize local variable
+        String checkPresence = attempt;
+        String checkPosition = attempt;
+        String referencePresence = secret;
+        String referencePosition = secret;
+        String goodPositions = "";
+        String presents = "";
+        String result = "";
+        String[][] langSubstitutes;
+        Response computerResponse = new Response();
+        Integer[] digitParameters = new Integer[]{this.digitsInGame,0,this.digitMaxRepeat};
+
+        debugV3("error management with own exceptions"); // comment used for debug
+        if (! StringTool.match(attempt, "^[0-" + (this.digitsInGame - 1) + "]{" + this.codeLength + "}$" ) ||
+                ! StringTool.matchSpecificDigitRule(attempt, "digitMaxRepeat", digitParameters))
+            invalidArgument("generateEvaluation","attempt");
+        if (! StringTool.match(secret, "^[0-" + (this.digitsInGame - 1) + "]{" + this.codeLength + "}$" ))
+            invalidArgument("generateEvaluation","secret");
+
+        if (GameCache.isFailed()) return computerResponse;
+
+        debugV3("The two strings are compared and return evaluation with symbols x and o"); // comment used for debug
+        for (int i = 0; i < checkPosition.length(); i ++){
+            if (checkPosition.charAt(i) == referencePosition.charAt(i)){
+                goodPositions += "x";
+                checkPresence = checkPresence.replaceFirst(String.valueOf(checkPosition.charAt(i)), "");
+                referencePresence = referencePresence.replaceFirst(String.valueOf(checkPosition.charAt(i)), "");
+            }
+        }
+        debugV2(lang("debug.mastermindGoodPosition") + goodPositions);
+        for (int i = 0; i < checkPresence.length(); i ++){
+            if (referencePresence.contains(String.valueOf(checkPresence.charAt(i)))) {
+                presents += "o";
+                referencePresence = referencePresence.replaceFirst(String.valueOf(checkPresence.charAt(i)), "");
+            }
+        }
+        debugV2(lang("debug.mastermindWrongPosition") + presents);
+
+        debugV3("Sort symbols in evaluation string"); // comment used for debug
+        result = goodPositions + presents;
+
+        debugV3("Set response properties and return response"); // comment used for debug
+        computerResponse.appendValue("evaluation", result);
+        computerResponse.appendValue("goodPosition", "" + goodPositions);
+        computerResponse.appendValue("presents", "" + presents);
+        langSubstitutes = new String[][] {{"VAR_RESULT_EVALUATION",result},
+                {"VAR_RIGHT_POSITION", "" + goodPositions.length()},
+                {"VAR_WRONG_POSITION", "" + presents.length()}};
+        computerResponse.setMessage(lang("mastermind.resultComputerEvaluation", langSubstitutes));
+        computerResponse.setSuccess(StringTool.match(result, "^x{"+ this.codeLength + "}$"));
+        return computerResponse;
+    }
+
+    @Override
+    public Response askPlayerEvaluation() {
+        if (GameCache.isFailed()) return null;
+
+        String color = "reset";
+        if (GameCache.isDefender() && GameCache.isChallenger()) color = PropertyHelper.config("color.defender");
+        String [][] langSubstitutes = new String[][] {{"VAR_PLAYER_SECRET", GameCache.getPlayerCode()}};
+        // Initialize local variable
+        String question = lang("mastermind.playerEvaluation", langSubstitutes);
+        String codePattern = "^[xo-]{0," + this.codeLength + "}$";
+        Response response = new Response();
+
+        debugV3("Get player input with UserInteraction scanner"); // comment used for degug
+        String evaluation = UserInteraction.promptInput(question, codePattern, color, false);
+        evaluation = evaluation.replaceAll("-","");
+
+        debugV3("Set response properties and return response"); // comment used for degug
+        response.appendValue("evaluation", evaluation);
+        return response;
+    }
+
+    @Override
+    public Response askRefereeControl(String submittedEvaluation, String refereeEvaluation) {
+        //if (GameCache.isFailed()) return null;
+        // Initialize local variable
+        Response response = new Response();
+        String playerString ;
+        String computerString ;
+        String message = "";
+        int attempts = Integer.parseInt(PropertyHelper.config("game.attempts"));
+        boolean isSimilarEvaluation = true;
+        Integer[] digitParameters = new Integer[]{this.digitsInGame,0,this.digitMaxRepeat};
+
+        debugV3("error management if arguments are wrong"); // comment used for debug
+        if (! StringTool.match(submittedEvaluation, "^[ox]{0," + this.codeLength + "}$" ) ||
+                ! StringTool.matchSpecificDigitRule(submittedEvaluation, "digitMaxRepeat", digitParameters))
+            invalidArgument("askRefereeControl","submittedEvaluation");
+        if (! StringTool.match(refereeEvaluation, "^[ox]{0," + this.codeLength + "}$" ))
+            invalidArgument("askRefereeControl","refereeEvaluation");
+
+        if (GameCache.isFailed()) return response;
+
+        debugV3("compare the two evalutions character by character to prevent different way of writing"); // comment used for debug
+        playerString = submittedEvaluation.replaceAll("o", "");
+        computerString = refereeEvaluation.replaceAll("o","");
+        String goodPositions = "" + computerString.length();
+        if (! playerString.contentEquals(computerString)) isSimilarEvaluation= false;
+        playerString = submittedEvaluation.replaceAll("x", "");
+        computerString = refereeEvaluation.replaceAll("x","");
+        String presents = "" + computerString.length();
+        if (! playerString.contentEquals(computerString)) isSimilarEvaluation= false;
+        String[][] langSubstitutes = new String[][] {{"VAR_PLAYER_EVALUATION", submittedEvaluation},
+                {"VAR_REFEREE_EVALUATION", refereeEvaluation},
+                {"VAR_RIGHT_POSITION", goodPositions},
+                {"VAR_WRONG_POSITION", presents}};
+
+        debugV3("following result, set message and success in response and return response"); // comment used for debug
+        if (! isSimilarEvaluation) {
+            debugV3("Player didn't give the right evaluation"); // comment used for debug
+            message += "%n" + lang("game.refereeWrongEvaluation", langSubstitutes);
+            response.setSuccess(false);
+        } else {
+            boolean computerFindCode = StringTool.match(refereeEvaluation,"^x{" + this.codeLength + "}$");
+            if ( computerFindCode ) {
+                debugV3("Referee confirm that computer find the secret code"); // comment used for debug
+                message += "%n"  + lang("game.computerFoundCode");
+                response.setSuccess(false);
+            } else if ( ! computerFindCode && GameCache.turn() == (attempts - 1)) {
+                message += "%n" + lang("game.computerDidntFindCode");
+                GameCache.wins();
+            } else {
+                message += lang("game.refereeAgree", langSubstitutes);
+            }
+        }
+        response.setMessage(message);
+        return response;
+    }
+
 }
--- src/test/java/com/herve/ocgames/mastermind/CodeCheckerMasterMindTest.java
+++ src/test/java/com/herve/ocgames/mastermind/CodeCheckerMasterMindTest.java
@@ -3,5 +3,66 @@ package com.herve.ocgames.mastermind;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CodeCheckerMasterMindTest {
+    /*
+     * Tests de la methode
+
+    @Test
+    public void Given_RightAttemptOn5DigitAnd8Chips_When_GenerateEvaluation_Then_Returnxxxxx() {
+        int[] gameLevel = {5,12,8,2};
+        CodeChecker codeChecker = new CodeCheckerMasterMind(gameLevel);
+        Response response = new Response();
+        response = codeChecker.generateEvaluation("42216", "42216");
+        assertEquals("xxxxx", response.getValue("evaluation"));
+    }
+
+    @Test
+    public void Given_Attempt3441AndSecret3304_When_generateEvaluation_Then_ResponseGetEvalutionxo() {
+        int[] gameLevel = {4,12,6,2};
+        CodeChecker codeChecker = new CodeCheckerMasterMind(gameLevel);
+        Response response = new Response();
+        response = codeChecker.generateEvaluation("3441", "3304");
+        assertEquals("xo", response.getValue("evaluation"));
+    }
+
+    @Test
+    public void Given_RightAttemptOn6DigitAnd10Chips_When_GenerateEvalution_Then_Returnxxxxx() {
+        int[] gameLevel = {6,12,10,2};
+        CodeChecker codeChecker = new CodeCheckerMasterMind(gameLevel);
+        Response response = new Response();
+        response = codeChecker.generateEvaluation("367119", "367119");
+        assertEquals("xxxxxx", response.getValue("evaluation"));
+    }
+
+    @Test
+    public void Given_InexactAttemptOn4Digit6Chips_When_GenerateEvaluation_Then_GiveEvalutionWithxo() {
+        int[] gameLevel = {4,12,6,2};
+        CodeChecker codeChecker = new CodeCheckerMasterMind(gameLevel);
+        Response response = new Response();
+        response = codeChecker.generateEvaluation("4202", "5521");
+        assertEquals("o", response.getValue("evaluation"));
+    }
+
+    @Test
+    public void Given_AttemptWithLetters_When_GenerateEvaluation_Then_ThrowInvalidAttemptException() {
+        int[] gameLevel = {4,12,6,2};
+        CodeChecker codeChecker = new CodeCheckerMasterMind(gameLevel);
+        //Response response = new Response();
+        //response = codeChecker.generateEvaluation("4ee2", "5521");
+        assertThrows(InvalidAttempt.class, () -> codeChecker.generateEvaluation("4ee2", "5521"));
+        //assertEquals("o", response.getValue("evaluation"));
+    }
+
+
+
+
+    @Test
+    public void Given_AttemptWithMoreIdenticalDigitsThanLimit_When_generateEvalution_Then_ThrowInvalidAttemptException() {
+        int[] gameLevel = {4,12,6,2};
+        CodeChecker codeChecker = new CodeCheckerMasterMind(gameLevel);
+        Response response = new Response();
+        response = codeChecker.generateEvaluation("4442", "5521");
+        assertEquals("o", response.getValue("evaluation"));
+    }
+     */
 
 }
