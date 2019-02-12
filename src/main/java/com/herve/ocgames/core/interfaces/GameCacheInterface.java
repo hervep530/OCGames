93d2509907fa6aaf4a97b38ed7a392a3201eb9b0
hervep530 <hervep6963@gmail.com>
2019-02-12 16:34:15 +0100

    add javadoc in GameCacheInterface and PlayerInterface

--- src/main/java/com/herve/ocgames/core/interfaces/GameCacheInterface.java
+++ src/main/java/com/herve/ocgames/core/interfaces/GameCacheInterface.java
@@ -4,12 +4,19 @@ import java.util.Map;
 
 public interface GameCacheInterface {
 
+    /*
+     * GameCache is a dynamic repository, it will be used like a small database to store volatiles game's data
+     */
+
 
     /**
-     *
+     * Initialize Object to prevent Null Pointer errors - replace constructor in static context
      */
     void initialize();
 
+    /*
+     * Getter and Setters
+     */
     String getName();
 
     int getTurn();
@@ -28,47 +35,47 @@ public interface GameCacheInterface {
     void setToLooser();
 
     /**
-     * A getter doesn't follow traditional name's rule
+     * A getter doesn't follow traditional name's rule - store different game's plans for computer attempt (not used in this version)
      */
     Map<String,String> strategies();
 
     /**
-     * This kind of setter doesn't follow traditional name's rule
+     * This kind of setter for array which store computer attempts -  doesn't follow traditional name's rule
      */
     void addComputerAttempt(String attempt);
 
     /**
-     * A getter doesn't follow traditional name's rule
+     * A getter for array which store computer attempts - doesn't follow traditional name's rule
      */
     String[] computerAttempts();
 
     /**
-     * This kind of setter doesn't follow traditional name's rule
+     * This kind of setter for array which store player attempts -  doesn't follow traditional name's rule
      */
     void addPlayerAttempt(String attempt);
 
     /**
-     * A getter doesn't follow traditional name's rule
+     * A getter for array which store player attempts - doesn't follow traditional name's rule
      */
     String[] playerAttempts();
 
     /**
-     * This kind of setter doesn't follow traditional name's rule
+     * This kind of setter for array which store computer evaluation -  doesn't follow traditional name's rule
      */
     void addComputerEvaluation(String evaluation);
 
     /**
-     * A getter doesn't follow traditional name's rule
+     * A getter for array which store computer evalution - doesn't follow traditional name's rule
      */
     String[] computerEvaluations();
 
     /**
-     * This kind of setter doesn't follow traditional name's rule
+     * This kind of setter for array which store player evaluation -  doesn't follow traditional name's rule
      */
     void addPlayerEvaluation(String evaluation);
 
     /**
-     * A getter doesn't follow traditional name's rule
+     * A getter for array which store player evaluation - doesn't follow traditional name's rule
      */
     String[] playerEvaluations();
 
--- src/main/java/com/herve/ocgames/core/interfaces/PlayerInterface.java
+++ src/main/java/com/herve/ocgames/core/interfaces/PlayerInterface.java
@@ -5,17 +5,35 @@ public interface PlayerInterface {
     /*
      * All mandatory methods for player are actions (sometimes passives) from player
      */
-    
+
+    /**
+     * Passive action from player - as if he asks computer to generate its secret code
+     */
     void letComputerGenerateSecretCode();
 
+    /**
+     * Action from player - he choose a secret code and submit it for referee control at each evaluation from player
+     */
     void submitSecretCode();
 
+    /**
+     * action from player - he give a new attempt to guess secret code and submit it for evaluation from computer
+     */
     void submitAttempt();
 
+    /**
+     * Passive action from player - as if he asks computer to give answer from his new attempt
+     */
     void getComputerEvaluation();
 
+    /**
+     * Passive action from player - as if he asks computer to give a new attempt
+     */
     void getComputerAttempt();
 
+    /**
+     * Action from player - he gives answer to computer after computer attempt
+     */
     void submitEvaluation();
 
 }
