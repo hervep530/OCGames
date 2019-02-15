226d69595ca7c17cdafb5c4ac3187b16b5982469
hervep530 <hervep6963@gmail.com>
2019-02-15 09:32:53 +0100

    remove src/test

--- src/test/java/com/herve/ocgames/mastermind/DigitToolMasterMindTest.java
+++
@@ -1,7 +0,0 @@
-package com.herve.ocgames.mastermind;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class DigitToolMasterMindTest {
-
-}
--- src/test/java/com/herve/ocgames/plusmoins/CodeCheckerPlusMoinsTest.java
+++
@@ -1,7 +0,0 @@
-package com.herve.ocgames.plusmoins;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CodeCheckerPlusMoinsTest {
-
-}
